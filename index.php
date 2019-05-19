49c9d45d58f810231766a5b7ef7ba439f5035d9c
Santhosh Chami <test@localhost.local>
2019-05-19 17:43:47 +0000

    updated content

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World! test"; ?>
+<?php echo "Hello World! test new"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
