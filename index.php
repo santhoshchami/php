b19b93b3526529434013d105317aaf02f9038b03
Santhosh Chami <test@localhost.local>
2019-05-21 19:54:55 +0000

    changed index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World! 20-05"; ?>
+<?php echo "Hello World! 21-05"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
